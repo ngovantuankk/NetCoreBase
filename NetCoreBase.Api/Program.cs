b013e7953f3f79fcbb429b25796cfd0fd0ce518d
Tuan Ngo <tuanngo.hv@gmail.com>
2024-11-25 10:28:09 +0700

    Error handling via error endpoint + custom problem factory

--- NetCoreBase.Api/Program.cs
+++ NetCoreBase.Api/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using NetCoreBase.Api.Errors;
 using NetCoreBase.Application;
 using NetCoreBase.Infrastructure;
 
@@ -9,10 +11,12 @@ var builder = WebApplication.CreateBuilder(args);
         .AddInfrastructure(builder.Configuration);
 
     builder.Services.AddControllers();
+    builder.Services.AddSingleton<ProblemDetailsFactory, NetCoreBaseProblemDetailsFactory>();
 }
 
 var app = builder.Build();
 {
+    app.UseExceptionHandler("/error");
     app.UseHttpsRedirection();
     app.MapControllers();
     app.Run();
